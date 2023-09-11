8e6be323590d471c354a5209a502c4d6a92d44db
luttism <luciana.mofatto@gmail.com>
2023-09-10 22:29:16 -0300

    Adicionando rotas usando react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
-// import Login from "./components/pages/Login";
-// import RegisterForm from "./components/pages/RegisterForm";
-// import ForgotPasswordForm from "./components/pages/ForgotPasswordForm";
-// import Courses from "./components/pages/Courses";
-// import CoursePage from "./components/pages/CoursePage";
+import Login from "./components/pages/Login";
+import RegisterForm from "./components/pages/RegisterForm";
+import ForgotPasswordForm from "./components/pages/ForgotPasswordForm";
+import Courses from "./components/pages/Courses";
+import CoursePage from "./components/pages/CoursePage";
 
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
@@ -13,12 +13,25 @@ import "./styles/main.css";
 
 
 function App() {
-  return <Home />;
+  //return <Home />;
   //return <Login />;
   //return <RegisterForm />;
   //return <ForgotPasswordForm />;
   //return <Courses />;
   //return <CoursePage />;
+  return (
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/registerform" element={<RegisterForm />} />
+    <Route path="/login/forgotpassword" element={<ForgotPasswordForm />} />
+    <Route path="/courses" element={<Courses />} />
+    <Route path="/courses/coursepage" element={<CoursePage />} />
+    <Route path="*" element={<h1>Not found!</h1>} />
+  </Routes>
+  </BrowserRouter>
+  );
 //   <BrowserRouter>
 //   <Routes>
 //     <Route path="/" element={<Home />} />
@@ -29,7 +42,7 @@ function App() {
 //     <Route path="/courses/coursepage" element={<CoursePage />} />
 //     <Route path="*" element={<h1>Not found!</h1>} />
 //   </Routes>
-// </BrowserRouter>
+// </BrowserRouter>;
 }
 
 export default App;
